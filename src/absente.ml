8cf3427727cc096e96d0f0a68efc683efafa35c4
desumn <ilyess.tahar1505@gmail.com>
2021-07-26 19:01:18 +0200

    shorter dummy string for a dummy commit.

--- src/absente.ml
+++ src/absente.ml
@@ -1,4 +1,5 @@
 
-let dummy_print () = print_endline "I can write to stdout... I'm the only one that can... Apart from print_endline or any other useful library functions that you have at your disposal."
+let dummy_print () = print_endline "I can write to stdout... Like you."
+
 
 
