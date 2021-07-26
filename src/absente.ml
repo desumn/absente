0ce70c1fd5e2fdd38547d89c9dadad5c62924dcb
desumn <yugamii.43@gmail.com>
2021-07-26 18:56:13 +0200

    created the basic project infrastructure.

---
+++ src/absente.ml
@@ -0,0 +1,4 @@
+
+let dummy_print () = print_endline "I can write to stdout... I'm the only one that can... Apart from print_endline or any other useful library functions that you have at your disposal."
+
+
